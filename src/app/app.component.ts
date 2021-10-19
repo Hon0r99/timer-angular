b08c8981ef6d85712e5b61ac477938f39a5fda6f
Hon0r99 <pose1don.dance@gmail.com>
2021-10-19 16:31:17 +0300

    Speed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable, fromEvent } from 'rxjs'
+import { buffer, debounceTime, map, filter} from 'rxjs/operators';
+import { interval } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular';
+  title = "CodeSandbox";
+
+  timeValue:string = `00:00:00`
+  time:number = 0;
+  started:boolean = false;
+
+  ngAfterViewInit() {
+    const source = interval(1000);
+    
+    const subscription = source.subscribe((x:any) => {
+      if (!this.started) return;
+      this.time++;
+      console.log( this.time);
+      this.timeValue =
+        ('0' + Math.floor((this.time / 360) % 60)).slice(-2) +
+        ':' +
+        ('0' + Math.floor((this.time / 60) % 60)).slice(-2) +
+        ':' +
+        ('0' + Math.floor(this.time % 60)).slice(-2);
+    });
+
+    fromEvent(document.querySelectorAll('#startBtn'), 'click').subscribe((e) => {
+      this.started = true;
+    });
+    fromEvent(document.querySelectorAll('#stopBtn'), 'click').subscribe((e) => {
+      this.started = false;
+      this.time = 0;
+      this.timeValue = `00:00:00`
+    });
+    fromEvent(document.querySelectorAll('#resetBtn'), 'click').subscribe((e) => {
+      this.time = 0;
+      this.timeValue = `00:00:00`
+    });
+
+    const mouse$ = fromEvent(document.querySelectorAll('#waitBtn'), 'click')
+
+    const buff$ = mouse$.pipe(
+      debounceTime(250),
+    )
+
+    const click$ = mouse$.pipe(
+      buffer(buff$),
+      map(list => {
+        return list.length;
+      }),
+      filter(x => x === 2),
+    )
+
+    click$.subscribe(() => {
+      this.started = false;
+    })
+      
+  }  
 }
